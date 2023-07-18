de2b75788f6763caf8e6be8f85522cdeb92fa0d8
Tomtoxi44 <tommy.angibaud@gmail.com>
2023-07-18 20:53:07 +0200

    Mise a jour du template

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,16 @@
 const express = require('express')
+require("dotenv").config();
+const app = express()
 
-const app = express();
+
+const port = parseInt(process.env.APP_PORT ?? "6000", 10);
 
 
-app.listen(port,(err)=> {
-if (err) {
-	console.error("Erreur de connexion");
-}else {
-console.log(`Server is listening on ${port}`);
-}
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    // eslint-disable-next-line no-restricted-syntax
+    console.log(`Server is listening on ${port}`);
+  }
 });
--- Frontend/src/03.globals/01.Pages/HomePage.jsx
+++ Frontend/src/03.globals/01.Pages/HomePage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage = () => {
     return (
         <div>
-            
+            <p>hello</p>
         </div>
     );
 };
